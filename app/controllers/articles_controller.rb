bf3d1bc897db40c8738da4219396500319f6b239
sudheer <sudheer.p276@gmail.com>
2018-11-30 10:28:39 +0530

    created edit template, action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,33 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def create
        # render plain: params[:article].inspect
        @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
+       if @article.save
+          flash[:notice]="Article was sucessfully created."
+          redirect_to article_path(@article)
+       else
+          render 'new'
+       end
+    end
+
+    def edit
+        @article= Article.find(params[:id])
+    end
+
+    def update
+        @article= Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice]= "Article was successfully updated."
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
 
     private
