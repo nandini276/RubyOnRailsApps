9b008895c9cae2a680fce867d4c84f432b8ab677
sudheer <sudheer.p276@gmail.com>
2018-12-07 14:18:23 +0530

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController <ApplicationController
     before_action :set_users, only:[ :edit, :update, :show, :destroy ]
+    def index
+        @users=User.all
+    end
     def new
         @user = User.new
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
-    def gravatar_for(user)
-        puts user.email.downcase
-        gravatar_id= Digest::MD5::hexdigest(user.email.downcase)
-        puts gravatar_id
-        gravatar_url="https://secure.gravatar.com/avatar/#{gravatar_id}"
-        puts gravatar_url
+    def gravatar_for(user, options = { size: 80})
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: "img-circle")
-        
-    end
+     end
 end
